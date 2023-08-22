f49b607119e8aa614b63bc8136f12434751023f3
shwaaaa <shwaa1026@naver.com>
2023-08-22 19:46:26 +0900

    login 화면 퍼블리싱

---
+++ Weeing/Colors/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Weeing
+//
+//  Created by 이승화 on 2023/08/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColor = Color("backgroundColor")
+    static let loginColor = Color("loginColor")
+    static let grayColor = Color("grayColor")
+    static let loginTextFieldColor = Color("loginTextFieldColor")
+}
+
---
+++ Weeing/Views/swiftUI.swift
@@ -0,0 +1,20 @@
+//
+//  swiftUI.swift
+//  Weeing
+//
+//  Created by 이승화 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct swiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct swiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        swiftUI()
+    }
+}
