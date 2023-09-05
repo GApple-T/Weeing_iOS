99867b242eaa0c40d8b4a9889011b456ad597f0f
한재형 <wjdtjffl55@naver.com>
2023-09-05 20:37:04 +0900

    Font.custom 변경

--- Weeing/Screen/MainScreen/View/MainView.swift
+++ Weeing/Screen/MainScreen/View/MainView.swift
@@ -8,48 +8,8 @@
 import SwiftUI
 
 struct MainView: View {
-    @State var date = Date()
     var body: some View {
-        ZStack{
-            Color.backgroundColor.ignoresSafeArea()
-            VStack{
-                HStack{
-                    Text("Weeing")
-                        .font(Font.custom("Nunito-Black", size: 40))
-                        .foregroundColor(Color.loginColor)
-                        .padding(.leading, 30)
-                    Spacer()
-                }
-                Image("Wee")
-                    .resizable()
-                    .frame(width: 360, height: 240)
-                    .cornerRadius(20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .inset(by: 0.5)
-                            .stroke(Color.loginColor, lineWidth: 1)
-                    )
-                    .padding(.bottom, 23)
-
-                ZStack{
-                    RoundedRectangle(cornerRadius: 8)
-                        .foregroundColor(Color.loginTextFieldColor)
-                        .frame(width: 360, height: 360)
-                    DatePicker(
-                        "DatePicker",
-                        selection: $date,
-                        displayedComponents: [.date]
-                    )
-                    .datePickerStyle(.graphical)
-                    .background(Color.loginTextFieldColor)
-                    .cornerRadius(8)
-                    .frame(width: 340, height: 340)
-                    .accentColor(.orange)
-                }
-                
-                Spacer()
-            }
-        }
+        screenTapBar()
     }
 }
 
