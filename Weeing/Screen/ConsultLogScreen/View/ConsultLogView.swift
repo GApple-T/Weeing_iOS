e081970685da047e4c11e005fd14d3cbc2e294be
“xixn2” <ssbkh0802@naver.com>
2023-10-10 10:06:37 +0900

    Func으로 코드 간결하게 수정

--- Weeing/Screen/ConsultLogScreen/View/ConsultLogView.swift
+++ Weeing/Screen/ConsultLogScreen/View/ConsultLogView.swift
@@ -21,84 +21,30 @@ struct ConsultLogView: View {
 
                 VStack(spacing: 0) {
 
-                        Text("Weeing")
-                            .font(.custom("Nunito-Black", size: 30))
-                            .foregroundColor(Color.P30)
-                            .padding(.trailing, 240)
-                            .padding(.bottom, 10)
-                            .padding(.top, 55)
+                
+                    Text("상담예약")
+                        .font(.system(size: 27))
+                        .fontWeight(.bold)
+                            .padding(.trailing, 250)
+                            .padding(.bottom, 30)
+                            .padding(.top, 058)
                     
                     
                     ScrollView(showsIndicators: false) {
                         LazyVStack {
-                            HStack {
-                                Text("상담예약")
-                                    .font(.system(size: 27))
-                                    .fontWeight(.bold)
-                                Spacer()
-                            }
-                            .padding(.horizontal, 23)
-                            
                             Button(action: {
                                 isShowingInitialPopup.toggle()
                             }) {
-                                VStack(alignment: .leading) {
-                                    Text("2023.11.21 (7교시) 상담 신청")
-                                        .font(.system(size: 18).bold())
-                                        .foregroundColor(Color("P30"))
-                                        .padding(.top, -8)
-                                    Text("상담사유")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 13).bold())
-                                    Text("요즘 너무 힘드러용... 전공도 그렇구 학교 생활이 너무 괴롭습니다 선생님... 저 좀 도와주세요 ㅠㅠ 진짜 잘하구 싶은데 잘 안됩니다ㅠㅠ 넘 힘들어요 버틸수 있을지 모르겠어요... 힘드네요.")
-                                        .font(.system(size: 10))
-                                        .foregroundColor(.gray)
-                                        .padding(.top, -5)
-                                        .multilineTextAlignment(.leading)
-                                }
-                                .padding(16)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.white)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .strokeBorder(Color("P30"), lineWidth: 1)
-                                        )
-                                )
+                                popupScreen(listColor: Color.P30)
                             }
 
                             ForEach(0 ..< 6, id: \.self) { _ in
-                                VStack(alignment: .leading) {
-                                    Text("2023.11.21 (6교시) 상담 신청")
-                                        .font(.system(size: 18).bold())
-                                        .foregroundColor(Color("N20"))
-                                        .padding(.top, -8)
-                                    Text("상담사유")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 13).bold())
-                                    Text("요즘 너무 힘드러용... 전공도 그렇구 학교 생활이 너무 괴롭습니다 선생님... 저 좀 도와주세요 ㅠㅠ 진짜 잘하구 싶은데 잘 안됩니다ㅠㅠ 넘 힘들어요 버틸수 있을지 모르겠어요... 힘드네요.")
-                                        .font(.system(size: 10))
-                                        .foregroundColor(.gray)
-                                        .padding(.top, -5)
-                                }
-                                .padding(16)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.white)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .strokeBorder(Color.gray, lineWidth: 1)
-                                        )
-                                )
+                                popupScreen(listColor: Color.gray)
                             }
                         }
                     }
-                    .background(Color("BG"))
                     .frame(height: 750)
-                    .listStyle(.plain)
-                    //.border(Color.gray, width: 1) // ScrollView 범위 확인
-
-                    Spacer()
+                    //.border(Color.gray)
                 }
             }
             .overlay(
@@ -112,7 +58,7 @@ struct ConsultLogView: View {
             .popup(isPresented: $isShowingInitialPopup) {
                 VStack(spacing: 0) {
                     Text("2023.11.21 (7교시) 상담")
-                        .font(.custom("AppleSDGothicNeoSB00", size: 16))
+                        .font(Font.custom("AppleSDGothicNeoSB00", size: 16))
                         .background(RoundedRectangle(cornerRadius: 10)
                             .foregroundColor(.white)
                         )
@@ -127,7 +73,7 @@ struct ConsultLogView: View {
                             
                         } label: {
                             Text("변경")
-                                .font(.custom("AppleSDGothicNeoSB00", size: 13))
+                                .font(Font.custom("AppleSDGothicNeoSB00", size: 13))
                                 .foregroundColor(.black)
                                 .frame(width: 150)
                         }
@@ -139,8 +85,8 @@ struct ConsultLogView: View {
                             isShowingCancelConfirmation = true
                         } label: {
                             Text("취소")
-                                .font(.custom("AppleSDGothicNeoSB00", size: 13))
-                                .foregroundColor(.red)
+                                .font(Font.custom("AppleSDGothicNeoSB00", size: 13))
+                                .foregroundColor(Color(red: 0.86, green: 0, blue: 0))
                                 .frame(width: 150)
                         }
                     }
@@ -152,24 +98,28 @@ struct ConsultLogView: View {
             }
             .popup(isPresented: $isShowingCancelConfirmation) {
                 VStack(spacing: 0) {
-                    Text("정말로 취소하시겠습니까?")
-                        .foregroundColor(.red)
-                        .font(.custom("AppleSDGothicNeoSB00", size: 16))
+                    Text("정말로 취소 하시겠습니까?")
+                        .font(Font.custom("AppleSDGothicNeoSB00", size: 16))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 0.86, green: 0, blue: 0))
+                        .frame(width: 300, alignment: .top)
                         .background(RoundedRectangle(cornerRadius: 10)
                             .foregroundColor(.white)
                         )
                         .padding(.top, 52)
+                        .lineLimit(nil)
                     Divider()
                         .frame(height: 2)
                         .padding(.top, 48)
                         .padding(.bottom, 12)
+
 
                     HStack {
                         Button {
                             isShowingCancelConfirmation = false
                         } label: {
                             Text("취소")
-                                .font(.custom("AppleSDGothicNeoSB00", size: 13))
+                                .font(Font.custom("AppleSDGothicNeoSB00", size: 13))
                                 .foregroundColor(.black)
                                 .frame(width: 150)
                         }
@@ -180,7 +130,7 @@ struct ConsultLogView: View {
                             isShowingCancellationComplete = true
                         } label: {
                             Text("확인")
-                                .font(.custom("AppleSDGothicNeoSB00", size: 13))
+                                .font(Font.custom("AppleSDGothicNeoSB00", size: 13))
                                 .foregroundColor(.black)
                                 .frame(width: 150)
                         }
@@ -194,7 +144,7 @@ struct ConsultLogView: View {
             .popup(isPresented: $isShowingCancellationComplete) {
                 VStack(spacing: 0) {
                     Text("상담 취소가 완료되었습니다.")
-                        .font(.custom("AppleSDGothicNeoSB00", size: 16))
+                        .font(Font.custom("AppleSDGothicNeoSB00", size: 16))
                         .background(RoundedRectangle(cornerRadius: 10)
                             .foregroundColor(.white)
                         )
@@ -208,7 +158,7 @@ struct ConsultLogView: View {
                         isShowingCancellationComplete = false
                     } label: {
                         Text("확인")
-                            .font(.custom("AppleSDGothicNeoSB00", size: 13))
+                            .font(Font.custom("AppleSDGothicNeoSB00", size: 13))
                             .foregroundColor(.black)
                             .frame(width: 320)
                     }
@@ -220,6 +170,32 @@ struct ConsultLogView: View {
             }
         }
     }
+}
+
+func popupScreen(listColor: Color) -> some View {
+    return VStack(alignment: .leading) {
+        Text("2023.11.21 (6교시) 상담 신청")
+            .font(.system(size: 18).bold())
+            .foregroundColor(listColor)
+            .padding(.top, -8)
+        Text("상담사유")
+            .foregroundColor(.black)
+            .font(.system(size: 13).bold())
+        Text("요즘 너무 힘드러용... 전공도 그렇구 학교 생활이 너무 괴롭습니다 선생님... 저 좀 도와주세요 ㅠㅠ 진짜 잘하구 싶은데 잘 안됩니다ㅠㅠ 넘 힘들어요 버틸수 있을지 모르겠어요... 힘드네요.")
+            .font(.system(size: 10))
+            .foregroundColor(.gray)
+            .padding(.top, -5)
+            .multilineTextAlignment(.leading)
+    }
+    .padding(16)
+    .background(
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(listColor, lineWidth: 1)
+            )
+    )
 }
 
 struct ConsultLogView_Previews: PreviewProvider {
