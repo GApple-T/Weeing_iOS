cd5f1ee7e24a2ea0ae9e730c221bc6e2126eb385
“xixn2” <ssbkh0802@naver.com>
2023-11-23 11:57:36 +0900

    주석 제거 및 시작화면을 Login창으로 변경

--- Weeing/WeeingApp.swift
+++ Weeing/WeeingApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct WeeingApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
-            // LoginView(viewModel: LoginViewModel())
+            LoginView(viewModel: LoginViewModel())
         }
     }
 }
